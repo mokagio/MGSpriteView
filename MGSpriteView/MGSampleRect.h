ed2a9cc3d6859c629e35f32a3e5e88420fe97bf8
Gio <giovanni.lodi42@gmail.com>
2013-05-29 00:41:54 +0100

    Added MGSampleRect, the DTO from the plist

---
+++ MGSpriteView/MGSampleRect.h
@@ -0,0 +1,26 @@
+//
+//  MGSampleRect.m
+//  MGSpriteView
+//
+//  Created by mokagio on 16/05/2013.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGSampleRect : NSObject
+
+@property (nonatomic, readonly, assign) CGRect bounds;
+@property (nonatomic, readonly, assign) CGRect contentRect;
+@property (nonatomic, readonly, assign) CGPoint offset;
+@property (nonatomic, readonly, assign) BOOL rotated;
+@property (nonatomic, readonly, assign) CGRect sourceColorRect;
+@property (nonatomic, readonly, assign) CGSize sourceSize;
+
+- (id)initWithImageSize:(CGSize)size
+                  frame:(CGRect)frame
+                 offset:(CGPoint)offset
+                rotated:(BOOL)rotated
+        sourceColorRect:(CGRect)sourceColorRect
+             sourceSize:(CGSize)sourceSize;
+
+@end
---
+++ MGSpriteView/MGSampleRect.m
@@ -0,0 +1,77 @@
+//
+//  MGSampleRect.m
+//  MGSpriteView
+//
+//  Created by mokagio on 16/05/2013.
+//
+
+#import "MGSampleRect.h"
+
+@interface MGSampleRect ()
+@property (nonatomic, assign) CGRect bounds;
+@property (nonatomic, assign) CGRect contentRect;
+@property (nonatomic, assign) CGPoint offset;
+@property (nonatomic, assign) BOOL rotated;
+@property (nonatomic, assign) CGRect sourceColorRect;
+@property (nonatomic, assign) CGSize sourceSize;
+@end
+
+@implementation MGSampleRect
+
+#pragma mark - Designated Initializer
+
+- (id)initWithImageSize:(CGSize)size
+                  frame:(CGRect)frame
+                 offset:(CGPoint)offset
+                rotated:(BOOL)rotated
+        sourceColorRect:(CGRect)sourceColorRect
+             sourceSize:(CGSize)sourceSize
+{
+    self = [super init];
+    if (self) {
+
+        CGFloat factor = [[UIScreen mainScreen] scale] == 1.0 ? 0.5 : 1.0;
+        self.rotated = rotated;
+
+        if (self.rotated) {
+            self.contentRect = CGRectMake(frame.origin.x * factor / size.width,
+                                          frame.origin.y * factor / size.height,
+                                          frame.size.height * factor / size.width,
+                                          frame.size.width * factor / size.height);
+            self.bounds = CGRectMake(0,
+                                     0,
+                                     frame.size.height * factor,
+                                     frame.size.width * factor);
+        } else {
+            self.contentRect = CGRectMake(frame.origin.x * factor / size.width,
+                                          frame.origin.y * factor / size.height,
+                                          frame.size.width * factor / size.width,
+                                          frame.size.height * factor / size.height);
+            self.bounds = CGRectMake(0,
+                                     0,
+                                     frame.size.width * factor,
+                                     frame.size.height * factor);
+        }
+        
+        // WHY + and - ? May be bec
+        self.offset = CGPointMake(offset.x * factor, -offset.y * factor);
+        
+        self.sourceColorRect = sourceColorRect;
+        self.sourceSize = sourceSize;
+    }
+    return self;
+}
+
+#pragma mark - NSObject
+
+- (id)init
+{
+    return [self initWithImageSize:CGSizeZero
+                             frame:CGRectZero
+                            offset:CGPointZero
+                           rotated:NO
+                   sourceColorRect:CGRectZero
+                        sourceSize:CGSizeZero];
+}
+
+@end
