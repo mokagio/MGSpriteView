67571c093ba38f61497fe2619fd3125a37d4b66f
Gio <giovanni.lodi42@gmail.com>
2013-05-29 01:13:09 +0100

    Added a simple sprite sheet based animation in the Sample

--- Sample/MGSpriteViewSample/MGSpriteViewSample/MGViewController.m
+++ Sample/MGSpriteViewSample/MGSpriteViewSample/MGViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "MGViewController.h"
+#import "MGSpriteView.h"
 
 @interface MGViewController ()
-
+@property (nonatomic, strong) MGSpriteView *squirtle;
 @end
 
 @implementation MGViewController
@@ -17,7 +18,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGFloat side = 30;
+    CGRect frame = CGRectMake((self.view.frame.size.width - side) / 2,
+                              (self.view.frame.size.height - side) / 2,
+                              side,
+                              side);
+    self.squirtle = [[MGSpriteView alloc] initWithFrame:frame
+                                             spriteSheetFileName:@"squirtle.png"
+                                                             fps:12];
+    [self.view addSubview:self.squirtle.view];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self.squirtle runAnimation];
 }
 
 - (void)didReceiveMemoryWarning
