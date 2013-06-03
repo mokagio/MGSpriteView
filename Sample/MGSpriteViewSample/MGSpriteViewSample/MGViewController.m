06916499fecb4645b8ac8b7ca79552dd7218c934
Gio <giovanni.lodi42@gmail.com>
2013-06-03 12:58:28 +0300

    Added charamander to the view

--- Sample/MGSpriteViewSample/MGSpriteViewSample/MGViewController.m
+++ Sample/MGSpriteViewSample/MGSpriteViewSample/MGViewController.m
@@ -12,13 +12,18 @@
 static const CGFloat kBulbasaurWidth = 49;
 static const CGFloat kBulbasaurHeight = 45;
 
+static const CGFloat kCharmanderWidth = 37;
+static const CGFloat kCharmanderHeight = 47;
+
 static const CGFloat kSquirtleWidth = 55;
 static const CGFloat kSquirtleHeight = 40;
 
 @interface MGViewController ()
 @property (nonatomic, strong) MGSpriteView *bulbasaur;
+@property (nonatomic, strong) MGSpriteView *charmander;
 @property (nonatomic, strong) MGSpriteView *squirtle;
 - (void)addBulbasaur;
+- (void)addCharmander;
 - (void)addSquirtle;
 @end
 
@@ -29,15 +34,18 @@ static const CGFloat kSquirtleHeight = 40;
     [super viewDidLoad];
     
     [self addBulbasaur];
+    [self addCharmander];
     [self addSquirtle];
     
-    self.bulbasaur.view.center = CGPointMake(100, 100);
-    self.squirtle.view.center = CGPointMake(200, 200);
+    self.bulbasaur.view.center = CGPointMake(50, 100);
+    self.charmander.view.center = CGPointMake(150, 200);
+    self.squirtle.view.center = CGPointMake(250, 300);
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [self.bulbasaur runAnimation];
+    [self.charmander runAnimation];
     [self.squirtle runAnimation];
 }
 
@@ -49,8 +57,16 @@ static const CGFloat kSquirtleHeight = 40;
     self.bulbasaur = [[MGSpriteView alloc] initWithFrame:frame
                                      spriteSheetFileName:@"bulbasaur.png"
                                                      fps:12];
-//    self.bulbasaur.view.backgroundColor = [UIColor redColor];
     [self.view addSubview:self.bulbasaur.view];
+}
+
+- (void)addCharmander
+{
+    CGRect frame = CGRectMake(0, 0, kCharmanderWidth, kCharmanderHeight);
+    self.charmander = [[MGSpriteView alloc] initWithFrame:frame
+                                     spriteSheetFileName:@"charmander.png"
+                                                     fps:12];
+    [self.view addSubview:self.charmander.view];
 }
 
 - (void)addSquirtle
